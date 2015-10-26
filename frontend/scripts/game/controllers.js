1af0ce9ec6db38fdc26594f6d29e352f24493537
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2015-10-26 23:07:42 +0100

    simple memory storage service

--- frontend/scripts/game/controllers.js
+++ frontend/scripts/game/controllers.js
@@ -3,7 +3,8 @@
 require("./gameTime");
 require("./gameData");
 module.exports = angular.module('Game.Controllers',['Game.Time','Game.Data'])
-.controller("Game.MainController", ['$scope', '$timeout', 'gameData','gameTime', function($scope, $timeout, gameData, gameTime){
+.controller("Game.MainController", ['$scope', '$timeout', 'gameData','gameTime','storage', function($scope, $timeout, gameData, gameTime, storage){
+  storage.init();
   gameData.init();
   gameTime.init();
   $scope.day = gameTime.getDay();
@@ -16,6 +17,8 @@ module.exports = angular.module('Game.Controllers',['Game.Time','Game.Data'])
   gameTime.addDayListener(function(time, day) {
     $scope.day = day;
     gameData.calculate();
+    storage.add("resource",day, JSON.parse(JSON.stringify(gameData.actualResources.getList())));
+    storage.add("buildings",day, JSON.parse(JSON.stringify(gameData.buildings.getList())));
   });
   gameTime.start();
 }])
--- frontend/scripts/services.js
+++ frontend/scripts/services.js
@@ -1,27 +1,25 @@
 /*jslint node: true */
 "use strict";
-
 module.exports = angular.module('EconomicGame.Services',[])
-.directive('ngEnter', function () {
-  return function (scope, element, attrs) {
-    element.bind("keydown keypress", function (event) {
-      if(event.which === 13) {
-        scope.$apply(function (){
-          scope.$eval(attrs.ngEnter);
-        });
-        event.preventDefault();
-      }
-    });
+/**
+ * temporary memory storage
+ */
+.service('storage', function () {
+  var memoryData = {};
+  this.init = function (){
+    memoryData = {};
   };
-})
-.directive('autoFocus', function($timeout) {
-  return {
-    restrict: 'AC',
-    link: function(_scope, _element) {
-      $timeout(function(){
-        _element[0].focus();
-      }, 0);
+  this.add = function(key, day, data){
+    if(!memoryData.key){
+      memoryData.key = [];
     }
+    memoryData.key[day] = data;
+  };
+  this.get = function(key){
+    if(!memoryData.key){
+      memoryData.key = [];
+    }
+    return memoryData.key;
   };
 });
 
